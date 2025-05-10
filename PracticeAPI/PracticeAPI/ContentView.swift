0e38b82c2da179b4a3e4be5c7733c4f6d4d4a6b0
iosアカデミア <yutamac2512@icloud.com>
2025-05-10 16:44:04 +0900

    Xcode最初の追加

---
+++ PracticeAPI/PracticeAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeAPI
+//
+//  Created by Engineer MacBook Air on 2025/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeAPI/PracticeAPI/PracticeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAPIApp.swift
+//  PracticeAPI
+//
+//  Created by Engineer MacBook Air on 2025/05/10.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
