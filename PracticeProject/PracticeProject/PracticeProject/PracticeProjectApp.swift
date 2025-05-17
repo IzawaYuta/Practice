977e5e0054dfe88f798fcf0739e116d69e596f7d
yutaizawa <yutamac2512@icloud.com>
2025-05-17 10:21:12 +0900

    通知許可申請を追加

--- PracticeProject/PracticeProject/PracticeProject/PracticeProjectApp.swift
+++ PracticeProject/PracticeProject/PracticeProject/PracticeProjectApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct PracticeProjectApp: App {
+    
+    // 初期化時に表示
+    init() {
+        requestNotificationAuthorization()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    // 通知許可申請をユーザに表示
+    func requestNotificationAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print("Authorization error: \(error.localizedDescription)")
+            } else {
+                print("Permission granted: \(granted)")
+            }
         }
     }
 }
