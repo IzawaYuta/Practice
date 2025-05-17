96fdf19321c22d2574b62c91f7951ed8239a858e
yutaizawa <yutamac2512@icloud.com>
2025-05-17 11:36:11 +0900

    ボタンタップでバナー通知実装

--- PracticeProject/PracticeProject/PracticeProject/ContentView.swift
+++ PracticeProject/PracticeProject/PracticeProject/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        QiitaView1()
+        NotificationView()
     }
 }
 
--- PracticeProject/PracticeProject/PracticeProject/Notification/NotificationView.swift
+++ PracticeProject/PracticeProject/PracticeProject/Notification/NotificationView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct NotificationView: View {
+    @State private var selectedTime = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            sendLocalPush()
+        }) {
+            Text("通知を出す")
+        }
+    }
+    
+    func sendLocalPush() {
+        // 1. 通知の内容を作成
+        let content = UNMutableNotificationContent()
+        content.title = "ローカル通知です"
+        content.subtitle = "こんにちは"
+        content.sound = .default // 音を指定
+        
+        // 2. 通知のトリガーを指定
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false) // 5秒後に通知
+        
+        // 3. 中身とトリガーでリクエストを作成
+        let request = UNNotificationRequest(identifier: "alerm_id", content: content, trigger: trigger)
+        
+        // 4. リクエストを追加
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { error in
+            if let error {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
